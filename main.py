a55b66249acdb1c2c20ecc96707e0412548af00c
Conor Warrilow <conrwar@hotmail.com>
2024-07-17 13:03:58 +0800

    added model preparation functions

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from CancerClassifier import logger
 from CancerClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from CancerClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -11,6 +13,19 @@ STAGE_NAME = "Data Ingestion Stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Base Model Preparatin Stage"
+
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = PrepareBaseModelTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
